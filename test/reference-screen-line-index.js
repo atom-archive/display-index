9e01a57e9938e6ec54179da654b8c7a2dedb3fb2
Nathan Sobo <nathan@github.com>
2016-02-16 13:13:49 -0700

    Simplify reference index
    Always seek to the leftmost token *inclusively* containing the target
    position. We can always move to successor in clients if we want
    exclusive behavior.

--- test/reference-screen-line-index.js
+++ test/reference-screen-line-index.js
@@ -1,4 +1,4 @@
-import {compare, traverse} from '../src/point-helpers'
+import {compare, traverse, minPoint} from '../src/point-helpers'
 
 export default class ReferenceScreenLineIndex {
   constructor () {
@@ -30,109 +30,112 @@ export default class ReferenceScreenLineIndex {
 
     return bufferPosition.row
   }
+
+  lineLengthForScreenRow (screenRow) {
+    return this.screenLines[screenRow].screenExtent
+  }
 }
 
 class ReferenceTokenIterator {
   constructor (screenLineIndex) {
     this.screenLineIndex = screenLineIndex
-    this.screenLines = screenLineIndex.screenLines
+    this.lineIterator = new ReferenceLineIterator(screenLineIndex)
   }
 
   seekToScreenPosition (targetPosition) {
+    let clippedTargetPosition = targetPosition
+
     if (targetPosition.row < 0) {
-      targetPosition = {row: 0, column: 0}
+      clippedTargetPosition = {row: 0, column: 0}
     }
     if (targetPosition.column < 0) {
-      targetPosition = {row: targetPosition.row, column: 0}
+      clippedTargetPosition = {row: targetPosition.row, column: 0}
     }
     if (targetPosition.row > this.getLastScreenRow()) {
-      targetPosition = {row: this.getLastScreenRow(), column: Infinity}
+      clippedTargetPosition = {row: this.getLastScreenRow(), column: Infinity}
     }
 
-    this.bufferStart = {row: 0, column: 0}
+    this.lineIterator.seekToScreenRow(clippedTargetPosition.row)
 
-    // find containing screen line
-    for (let screenRow = 0; screenRow < this.screenLines.length; screenRow++) {
-      this.screenLine = this.screenLines[screenRow]
-      if (screenRow === targetPosition.row) break
-      this.bufferStart = traverse(this.bufferStart, this.screenLine.bufferExtent)
-    }
+    clippedTargetPosition = minPoint(clippedTargetPosition, this.lineIterator.getScreenEnd())
 
-    this.screenStart = {row: targetPosition.row, column: 0}
+    this.screenStart = this.lineIterator.getScreenStart()
+    this.bufferStart = this.lineIterator.getBufferStart()
 
-    // find containing token
-    for (this.tokenIndex = 0; this.tokenIndex < this.screenLine.tokens.length; this.tokenIndex++) {
-      let token = this.screenLine.tokens[this.tokenIndex]
+    let tokens = this.lineIterator.getTokens()
+    for (this.tokenIndex = 0; this.tokenIndex < tokens.length; this.tokenIndex++) {
+      let token = tokens[this.tokenIndex]
       this.screenEnd = traverse(this.screenStart, {row: 0, column: token.screenExtent})
       this.bufferEnd = traverse(this.bufferStart, token.bufferExtent)
 
-      let tokenContainsTarget =
-        compare(this.screenStart, targetPosition) <= 0
-          && compare(targetPosition, this.screenEnd) < 0
-
-      if (tokenContainsTarget) return true
+      if (this.containsScreenPosition(clippedTargetPosition)) break
 
       this.screenStart = this.screenEnd
       this.bufferStart = this.bufferEnd
     }
 
-    return false
+    return this.containsScreenPosition(targetPosition)
   }
 
   seekToBufferPosition (targetPosition) {
+    let clippedTargetPosition = targetPosition
+
     if (targetPosition.row < 0) {
-      targetPosition = {row: 0, column: 0}
+      clippedTargetPosition = {row: 0, column: 0}
     }
     if (targetPosition.column < 0) {
-      targetPosition = {row: targetPosition.row, column: 0}
+      clippedTargetPosition = {row: targetPosition.row, column: 0}
+    }
+    if (targetPosition.row > this.getLastBufferRow()) {
+      clippedTargetPosition = {row: this.getLastBufferRow(), column: Infinity}
     }
 
-    let lastBufferRow = this.getLastBufferRow()
-    if (targetPosition.row > lastBufferRow) {
-      targetPosition = {row: lastBufferRow, column: Infinity}
-    }
+    this.lineIterator.seekToBufferPosition(clippedTargetPosition)
 
-    // find containing screen line
-    let lineBufferStart = {row: 0, column: 0}
-    let screenRow = 0
-    while (true) {
-      this.screenLine = this.screenLines[screenRow]
+    clippedTargetPosition = minPoint(clippedTargetPosition, this.lineIterator.getBufferEnd())
 
-      let lineBufferEnd = traverse(lineBufferStart, this.screenLine.bufferExtent)
-      let lineContainsTargetPosition =
-        compare(lineBufferStart, targetPosition) <= 0
-          && compare(targetPosition, lineBufferEnd) < 0
+    this.screenStart = this.lineIterator.getScreenStart()
+    this.bufferStart = this.lineIterator.getBufferStart()
 
-      if (lineContainsTargetPosition || screenRow === this.getLastScreenRow()) break
-
-      lineBufferStart = lineBufferEnd
-      screenRow++
-    }
-
-    this.screenStart = {row: screenRow, column: 0}
-    this.bufferStart = lineBufferStart
-
-    // find containing token
-    for (this.tokenIndex = 0; this.tokenIndex < this.screenLine.tokens.length; this.tokenIndex++) {
-      let token = this.screenLine.tokens[this.tokenIndex]
+    let tokens = this.lineIterator.getTokens()
+    for (this.tokenIndex = 0; this.tokenIndex < tokens.length; this.tokenIndex++) {
+      let token = tokens[this.tokenIndex]
       this.screenEnd = traverse(this.screenStart, {row: 0, column: token.screenExtent})
       this.bufferEnd = traverse(this.bufferStart, token.bufferExtent)
 
-      let tokenContainsTarget =
-        compare(this.bufferStart, targetPosition) <= 0
-          && compare(targetPosition, this.bufferEnd) < 0
-
-      if (tokenContainsTarget) return true
+      if (this.containsBufferPosition(clippedTargetPosition)) break
 
       this.screenStart = this.screenEnd
       this.bufferStart = this.bufferEnd
     }
 
-    return false
+    return this.containsBufferPosition(targetPosition)
+  }
+
+  moveToSuccessor () {
+    this.screenStart = this.screenEnd
+    this.bufferStart = this.bufferEnd
+    this.tokenIndex++
+
+    let token = this.getCurrentToken()
+    while (!token) {
+      if (this.lineIterator.moveToSuccessor()) {
+        this.screenStart = this.lineIterator.getScreenStart()
+        this.bufferStart = this.lineIterator.getBufferStart()
+        this.tokenIndex = 0
+        token = this.getCurrentToken()
+      } else {
+        return false
+      }
+    }
+
+    this.screenEnd = traverse(this.screenStart, {row: 0, column: token.screenExtent})
+    this.bufferEnd = traverse(this.screenStart, token.bufferExtent)
+    return true
   }
 
   getMetadata () {
-    let token = this.screenLine.tokens[this.tokenIndex]
+    let token = this.getCurrentToken()
     return token ? token.metadata : null
   }
 
@@ -152,11 +155,93 @@ class ReferenceTokenIterator {
     return this.bufferEnd
   }
 
+  getCurrentToken () {
+    let tokens = this.lineIterator.getTokens()
+    return tokens ? tokens[this.tokenIndex] : null
+  }
+
+  containsScreenPosition (screenPosition) {
+    return compare(this.screenStart, screenPosition) <= 0
+      && compare(screenPosition, this.screenEnd) <= 0
+  }
+
+  containsBufferPosition (bufferPosition) {
+    return compare(this.bufferStart, bufferPosition) <= 0
+      && compare(bufferPosition, this.bufferEnd) <= 0
+  }
+
   getLastScreenRow () {
     return this.screenLineIndex.getLastScreenRow()
   }
 
   getLastBufferRow () {
     return this.screenLineIndex.getLastBufferRow()
+  }
+}
+
+class ReferenceLineIterator {
+  constructor (screenLineIndex) {
+    this.screenLineIndex = screenLineIndex
+  }
+
+  seekToScreenRow (targetRow) {
+    this.bufferStart = {row: 0, column: 0}
+
+    let screenLineCount = this.screenLineIndex.screenLines.length
+    for (this.currentScreenRow = 0; this.currentScreenRow < screenLineCount; this.currentScreenRow++) {
+      this.bufferEnd = traverse(this.bufferStart, this.getCurrentScreenLine().bufferExtent)
+      if (this.currentScreenRow === targetRow) break
+      this.bufferStart = this.bufferEnd
+    }
+  }
+
+  seekToBufferPosition (targetPosition) {
+    this.bufferStart = {row: 0, column: 0}
+
+    let screenLineCount = this.screenLineIndex.screenLines.length
+    for (this.currentScreenRow = 0; this.currentScreenRow < screenLineCount; this.currentScreenRow++) {
+      this.bufferEnd = traverse(this.bufferStart, this.getCurrentScreenLine().bufferExtent)
+      if (this.containsBufferPosition(targetPosition)) break
+      if (this.currentScreenRow === screenLineCount - 1) break
+      this.bufferStart = this.bufferEnd
+    }
+  }
+
+  moveToSuccessor () {
+    this.bufferStart = this.bufferEnd
+    this.currentScreenRow++
+    let screenLine = this.getCurrentScreenLine()
+    if (!screenLine) return false
+    this.bufferEnd = traverse(this.bufferStart, screenLine.bufferExtent)
+  }
+
+  getScreenStart () {
+    return {row: this.currentScreenRow, column: 0}
+  }
+
+  getScreenEnd () {
+    return {row: this.currentScreenRow, column: this.getCurrentScreenLine().screenExtent}
+  }
+
+  getBufferStart () {
+    return this.bufferStart
+  }
+
+  getBufferEnd () {
+    return this.bufferEnd
+  }
+
+  getTokens () {
+    let screenLine = this.getCurrentScreenLine()
+    return screenLine ? screenLine.tokens : null
+  }
+
+  containsBufferPosition (bufferPosition) {
+    return compare(this.bufferStart, bufferPosition) <= 0
+      && compare(bufferPosition, this.bufferEnd) <= 0
+  }
+
+  getCurrentScreenLine () {
+    return this.screenLineIndex.screenLines[this.currentScreenRow]
   }
 }
--- test/reference-screen-line-index.test.js
+++ test/reference-screen-line-index.test.js
@@ -1,14 +1,14 @@
 import ReferenceScreenLineIndex from './reference-screen-line-index'
 
 describe('ReferenceScreenLineIndex', () => {
-  it('seeks token iterators via screen coordinates ', () => {
+  it('seeks to the leftmost token containing a given screen position', () => {
     let screenLineIndex = buildScreenLineIndex()
     let iterator = screenLineIndex.buildIterator()
 
     assert(iterator.seekToScreenPosition(point(0, 0)))
     assertIteratorState(iterator, point(0, 0), point(0, 5), point(0, 0), point(0, 5), 'a')
 
-    assert(iterator.seekToScreenPosition(point(0, 5)))
+    assert(iterator.seekToScreenPosition(point(0, 6)))
     assertIteratorState(iterator, point(0, 5), point(0, 6), point(0, 5), point(2, 5), 'b')
 
     assert(iterator.seekToScreenPosition(point(1, 2)))
@@ -17,27 +17,32 @@ describe('ReferenceScreenLineIndex', () => {
     assert(iterator.seekToScreenPosition(point(2, 2)))
     assertIteratorState(iterator, point(2, 0), point(2, 5), point(3, 10), point(3, 10), 'f')
 
-    assert(!iterator.seekToScreenPosition(point(2, 15)))
-    assertIteratorState(iterator, point(2, 15), point(2, 15), point(3, 20), point(3, 20), null)
+    assert(iterator.seekToScreenPosition(point(2, 15)))
+    assertIteratorState(iterator, point(2, 10), point(2, 15), point(3, 15), point(3, 20), 'h')
+  })
+
+  it('seeks to the closest token to an invalid screen position', () => {
+    let screenLineIndex = buildScreenLineIndex()
+    let iterator = screenLineIndex.buildIterator()
 
     assert(!iterator.seekToScreenPosition(point(3, 2)))
-    assertIteratorState(iterator, point(2, 15), point(2, 15), point(3, 20), point(3, 20), null)
+    assertIteratorState(iterator, point(2, 10), point(2, 15), point(3, 15), point(3, 20), 'h')
 
-    assert(iterator.seekToScreenPosition(point(-1, 2)))
+    assert(!iterator.seekToScreenPosition(point(-1, 2)))
     assertIteratorState(iterator, point(0, 0), point(0, 5), point(0, 0), point(0, 5), 'a')
 
-    assert(iterator.seekToScreenPosition(point(1, -2)))
+    assert(!iterator.seekToScreenPosition(point(1, -2)))
     assertIteratorState(iterator, point(1, 0), point(1, 5), point(3, 0), point(3, 5), 'd')
   })
 
-  it('seeks token iterators via buffer coordinates', () => {
+  it('seeks to the leftmost token containing a given buffer position', () => {
     let screenLineIndex = buildScreenLineIndex()
     let iterator = screenLineIndex.buildIterator()
 
     assert(iterator.seekToBufferPosition(point(0, 0)))
     assertIteratorState(iterator, point(0, 0), point(0, 5), point(0, 0), point(0, 5), 'a')
 
-    assert(iterator.seekToBufferPosition(point(0, 5)))
+    assert(iterator.seekToBufferPosition(point(0, 6)))
     assertIteratorState(iterator, point(0, 5), point(0, 6), point(0, 5), point(2, 5), 'b')
 
     assert(iterator.seekToBufferPosition(point(1, 1)))
@@ -46,11 +51,29 @@ describe('ReferenceScreenLineIndex', () => {
     assert(iterator.seekToBufferPosition(point(2, 7)))
     assertIteratorState(iterator, point(0, 6), point(0, 11), point(2, 5), point(2, 10), 'c')
 
-    assert(iterator.seekToBufferPosition(point(3, 10)))
+    assert(iterator.seekToBufferPosition(point(3, 11)))
     assertIteratorState(iterator, point(2, 5), point(2, 10), point(3, 10), point(3, 15), 'g')
 
-    assert(!iterator.seekToBufferPosition(point(3, 20)))
-    assertIteratorState(iterator, point(2, 15), point(2, 15), point(3, 20), point(3, 20), null)
+    assert(iterator.seekToBufferPosition(point(3, 20)))
+    assertIteratorState(iterator, point(2, 10), point(2, 15), point(3, 15), point(3, 20), 'h')
+
+    assert(iterator.seekToBufferPosition(point(3, 20)))
+    assertIteratorState(iterator, point(2, 10), point(2, 15), point(3, 15), point(3, 20), 'h')
+  })
+
+  it('seeks to the closest token to an invalid buffer position', () => {
+    let screenLineIndex = buildScreenLineIndex()
+    let iterator = screenLineIndex.buildIterator()
+
+    assert(!iterator.seekToBufferPosition(point(4, 2)))
+    assertIteratorState(iterator, point(2, 10), point(2, 15), point(3, 15), point(3, 20), 'h')
+    return
+
+    assert(!iterator.seekToBufferPosition(point(-1, 2)))
+    assertIteratorState(iterator, point(0, 0), point(0, 5), point(0, 0), point(0, 5), 'a')
+
+    assert(!iterator.seekToBufferPosition(point(1, -2)))
+    assertIteratorState(iterator, point(1, 0), point(1, 5), point(3, 0), point(3, 5), 'd')
   })
 })
 
@@ -89,7 +112,7 @@ function buildScreenLineIndex () {
     },
     { // soft wrap continuation
       screenExtent: 15,
-      bufferExtent: point(1, 0),
+      bufferExtent: point(0, 10), // last line of buffer
       tokens: [
         {screenExtent: 5, bufferExtent: point(0, 0), metadata: 'f'}, // hanging indent
         {screenExtent: 5, bufferExtent: point(0, 5), metadata: 'g'},
